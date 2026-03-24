4658caf6cb2cbb1ef693d3a20d2d65b420892c68
GoldForContinent <manukip642@gmail.com>
2026-03-24 12:19:39 +0300

    DEPLOYMENT

---
+++ api/index.java
@@ -0,0 +1,28 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class index {
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "3000"));
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Student Course Registration System is running!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+        
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port " + port);
+    }
+}
